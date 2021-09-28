cbf0882eb0f1353b566889829ea37454fd4ecada
KondrashinMikhail <90245664+KondrashinMikhail@users.noreply.github.com>
2021-09-28 11:27:16 +0400

    done second lab var 13 Kondrashin PI-21

--- WindowsFormsExcavator/WindowsFormsExcavator/BucketExcavavtor.cs
+++ WindowsFormsExcavator/WindowsFormsExcavator/BucketExcavavtor.cs
@@ -7,70 +7,24 @@ using System.Drawing;
 
 namespace WindowsFormsExcavator
 {
-    class Excavavtor
+    class BucketExcavavtor : Excavator
     {
-        private float startPosX;
-        private float startPosY;
-
-        private int _pictureWidth = 900;
-        private int _pictureHeight = 500;
-
-        private readonly int excavatorWidth = 150;
-        private readonly int excavatorHeight = 70;
-
-        public int MaxSpeed { private set; get; }
-        public float Weight { private set; get; }
-        public Color MainColor { private set; get; }
         public Color DopColor { private set; get; }
         public bool FrontBucket { private set; get; }
         public bool BackBucket { private set; get; }
 
-        public void Init(int maxSpeed, float weight, Color mainColor, Color dopColor, bool frontBucket, bool backBucket) {
-            MaxSpeed = maxSpeed;
-            Weight = weight;
-            MainColor = mainColor;
+        public BucketExcavavtor(int maxSpeed, float weight, Color mainColor, Color dopColor, bool frontBucket, bool backBucket):
+			base(maxSpeed, weight, mainColor, 170, 70)
+		{
             DopColor = dopColor;
             FrontBucket = frontBucket;
             BackBucket = backBucket;
         }
 
-        public void SetPosition(int x, int y, int width, int height) {
-            startPosX = width / 2 - excavatorWidth + x;
-            startPosY = height / 2 + excavatorHeight / 2 + y;
-        }
-
-        public void MoveExcavator(Direction direction) {
-            float step = MaxSpeed * 100 / Weight;
-            switch (direction) {
-                case Direction.Down:
-                    if (startPosY + step < _pictureHeight - excavatorHeight)
-                        startPosY += step;
-                    break;
-                
-				case Direction.Up:
-                    if (startPosY > 20)
-                        startPosY -= step;
-                    break;
-                case Direction.Left:
-                    if (startPosX > 0)
-                        startPosX -= step;
-                    break;
-                
-				case Direction.Right:
-                    if (startPosX + step < _pictureWidth - excavatorWidth)
-                        startPosX += step;
-                    break;
-            }
-        }
-
-		public void DrawExcavator(Graphics g)
+		public override void DrawExcavator(Graphics g)
 		{
 			Pen mainPen = new Pen(MainColor);
 			Pen dopPen = new Pen(DopColor);
-			Brush mainBrush = new SolidBrush(MainColor);
-			Brush dopBrush = new SolidBrush(DopColor);
-			Brush grayBrush = new SolidBrush(Color.Gray);
-			Brush blueBrush = new SolidBrush(Color.Blue);
 			if (FrontBucket)
 			{
 				//Ковш
@@ -113,17 +67,8 @@ namespace WindowsFormsExcavator
 				g.DrawLine(mainPen, startPosX - 10, startPosY - 10, startPosX - 10, startPosY);
 				g.DrawLine(mainPen, startPosX - 20, startPosY - 10, startPosX - 20, startPosY);
 			}
-			//Кабина
-			g.FillRectangle(blueBrush, startPosX + 40, startPosY - 20, 20, 20);
-			//Кузов
-			g.FillRectangle(mainBrush, startPosX, startPosY, 40, 20);
-			g.FillRectangle(dopBrush, startPosX + 40, startPosY, 20, 20);
-			g.DrawRectangle(mainPen, startPosX + 40, startPosY - 20, 20, 20);
-			//Гусеницы
-			g.FillEllipse(mainBrush, startPosX, startPosY + 20, 60, 20);
-			g.FillEllipse(grayBrush, startPosX, startPosY + 20, 20, 20);
-			g.FillEllipse(grayBrush, startPosX + 40, startPosY + 20, 20, 20);
-			g.FillEllipse(grayBrush, startPosX + 25, startPosY + 25, 10, 10);
+
+			base.DrawExcavator(g);
 		}
 	}
 }
---
+++ WindowsFormsExcavator/WindowsFormsExcavator/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsExcavator
+{
+    public abstract class Vehicle : ITransport
+    {
+        protected float startPosX;
+        protected float startPosY;
+        
+        protected int _pictureWidth = 900;
+        protected int _pictureHeight = 500;
+
+        public int MaxSpeed { protected set; get; }
+        public float Weight { protected set; get; }
+        public Color MainColor { protected set; get; }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            startPosX = width / 2 + x;
+            startPosY = height / 2 + y;
+        }
+
+        public abstract void MoveExcavator(Direction direction);
+        public abstract void DrawExcavator(Graphics g);
+
+    }
+}
