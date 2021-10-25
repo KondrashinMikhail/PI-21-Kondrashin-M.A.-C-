395525c5bfadd82a58680841db6662376633368c
KondrashinMikhail <90245664+KondrashinMikhail@users.noreply.github.com>
2021-10-25 11:47:21 +0400

    done fourth lab

--- WindowsFormsExcavator/WindowsFormsExcavator/ParkingForm.Designer.cs
+++ WindowsFormsExcavator/WindowsFormsExcavator/ParkingForm.Designer.cs
@@ -33,16 +33,21 @@ namespace WindowsFormsExcavator
             this.buttonParkingBucketExcavator = new System.Windows.Forms.Button();
             this.groupBoxGetExcavator = new System.Windows.Forms.GroupBox();
             this.buttonGetExcavator = new System.Windows.Forms.Button();
-            this.maskedTextBoxPlace = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
             this.labelPlace = new System.Windows.Forms.Label();
             this.pictureBoxParking = new System.Windows.Forms.PictureBox();
+            this.listBoxParkings = new System.Windows.Forms.ListBox();
+            this.buttonAddParking = new System.Windows.Forms.Button();
+            this.buttonDeletePaking = new System.Windows.Forms.Button();
+            this.textBoxNewLevelName = new System.Windows.Forms.TextBox();
+            this.labelParkings = new System.Windows.Forms.Label();
             this.groupBoxGetExcavator.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonParkingExcavator
             // 
-            this.buttonParkingExcavator.Location = new System.Drawing.Point(768, 12);
+            this.buttonParkingExcavator.Location = new System.Drawing.Point(708, 265);
             this.buttonParkingExcavator.Name = "buttonParkingExcavator";
             this.buttonParkingExcavator.Size = new System.Drawing.Size(104, 36);
             this.buttonParkingExcavator.TabIndex = 1;
@@ -52,7 +57,7 @@ namespace WindowsFormsExcavator
             // 
             // buttonParkingBucketExcavator
             // 
-            this.buttonParkingBucketExcavator.Location = new System.Drawing.Point(768, 54);
+            this.buttonParkingBucketExcavator.Location = new System.Drawing.Point(708, 307);
             this.buttonParkingBucketExcavator.Name = "buttonParkingBucketExcavator";
             this.buttonParkingBucketExcavator.Size = new System.Drawing.Size(104, 48);
             this.buttonParkingBucketExcavator.TabIndex = 2;
@@ -63,11 +68,11 @@ namespace WindowsFormsExcavator
             // groupBoxGetExcavator
             // 
             this.groupBoxGetExcavator.Controls.Add(this.buttonGetExcavator);
-            this.groupBoxGetExcavator.Controls.Add(this.maskedTextBoxPlace);
+            this.groupBoxGetExcavator.Controls.Add(this.maskedTextBox);
             this.groupBoxGetExcavator.Controls.Add(this.labelPlace);
-            this.groupBoxGetExcavator.Location = new System.Drawing.Point(768, 108);
+            this.groupBoxGetExcavator.Location = new System.Drawing.Point(708, 361);
             this.groupBoxGetExcavator.Name = "groupBoxGetExcavator";
-            this.groupBoxGetExcavator.Size = new System.Drawing.Size(112, 88);
+            this.groupBoxGetExcavator.Size = new System.Drawing.Size(104, 88);
             this.groupBoxGetExcavator.TabIndex = 3;
             this.groupBoxGetExcavator.TabStop = false;
             this.groupBoxGetExcavator.Text = "Забрать экскаватор";
@@ -82,12 +87,12 @@ namespace WindowsFormsExcavator
             this.buttonGetExcavator.UseVisualStyleBackColor = true;
             this.buttonGetExcavator.Click += new System.EventHandler(this.buttonGetExcavator_Click);
             // 
-            // maskedTextBoxPlace
+            // maskedTextBox
             // 
-            this.maskedTextBoxPlace.Location = new System.Drawing.Point(54, 32);
-            this.maskedTextBoxPlace.Name = "maskedTextBoxPlace";
-            this.maskedTextBoxPlace.Size = new System.Drawing.Size(27, 20);
-            this.maskedTextBoxPlace.TabIndex = 1;
+            this.maskedTextBox.Location = new System.Drawing.Point(54, 32);
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(27, 20);
+            this.maskedTextBox.TabIndex = 1;
             // 
             // labelPlace
             // 
@@ -102,15 +107,64 @@ namespace WindowsFormsExcavator
             // 
             this.pictureBoxParking.Location = new System.Drawing.Point(2, 1);
             this.pictureBoxParking.Name = "pictureBoxParking";
-            this.pictureBoxParking.Size = new System.Drawing.Size(750, 450);
+            this.pictureBoxParking.Size = new System.Drawing.Size(686, 462);
             this.pictureBoxParking.TabIndex = 4;
             this.pictureBoxParking.TabStop = false;
+            // 
+            // listBoxParkings
+            // 
+            this.listBoxParkings.FormattingEnabled = true;
+            this.listBoxParkings.Location = new System.Drawing.Point(708, 94);
+            this.listBoxParkings.Name = "listBoxParkings";
+            this.listBoxParkings.Size = new System.Drawing.Size(104, 95);
+            this.listBoxParkings.TabIndex = 5;
+            // 
+            // buttonAddParking
+            // 
+            this.buttonAddParking.Location = new System.Drawing.Point(708, 51);
+            this.buttonAddParking.Name = "buttonAddParking";
+            this.buttonAddParking.Size = new System.Drawing.Size(104, 37);
+            this.buttonAddParking.TabIndex = 6;
+            this.buttonAddParking.Text = "Добавить парковку";
+            this.buttonAddParking.UseVisualStyleBackColor = true;
+            this.buttonAddParking.Click += new System.EventHandler(this.buttonAddParking_Click);
+            // 
+            // buttonDeletePaking
+            // 
+            this.buttonDeletePaking.Location = new System.Drawing.Point(708, 195);
+            this.buttonDeletePaking.Name = "buttonDeletePaking";
+            this.buttonDeletePaking.Size = new System.Drawing.Size(104, 37);
+            this.buttonDeletePaking.TabIndex = 7;
+            this.buttonDeletePaking.Text = "Удалить парковку";
+            this.buttonDeletePaking.UseVisualStyleBackColor = true;
+            this.buttonDeletePaking.Click += new System.EventHandler(this.buttonDeletePaking_Click);
+            // 
+            // textBoxNewLevelName
+            // 
+            this.textBoxNewLevelName.Location = new System.Drawing.Point(708, 25);
+            this.textBoxNewLevelName.Name = "textBoxNewLevelName";
+            this.textBoxNewLevelName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNewLevelName.TabIndex = 8;
+            // 
+            // labelParkings
+            // 
+            this.labelParkings.AutoSize = true;
+            this.labelParkings.Location = new System.Drawing.Point(729, 9);
+            this.labelParkings.Name = "labelParkings";
+            this.labelParkings.Size = new System.Drawing.Size(60, 13);
+            this.labelParkings.TabIndex = 9;
+            this.labelParkings.Text = "Парковки:";
             // 
             // ParkingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.ClientSize = new System.Drawing.Size(832, 461);
+            this.Controls.Add(this.labelParkings);
+            this.Controls.Add(this.textBoxNewLevelName);
+            this.Controls.Add(this.buttonDeletePaking);
+            this.Controls.Add(this.buttonAddParking);
+            this.Controls.Add(this.listBoxParkings);
             this.Controls.Add(this.pictureBoxParking);
             this.Controls.Add(this.groupBoxGetExcavator);
             this.Controls.Add(this.buttonParkingBucketExcavator);
@@ -121,6 +175,7 @@ namespace WindowsFormsExcavator
             this.groupBoxGetExcavator.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -129,8 +184,13 @@ namespace WindowsFormsExcavator
         private System.Windows.Forms.Button buttonParkingBucketExcavator;
         private System.Windows.Forms.GroupBox groupBoxGetExcavator;
         private System.Windows.Forms.Button buttonGetExcavator;
-        private System.Windows.Forms.MaskedTextBox maskedTextBoxPlace;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
         private System.Windows.Forms.Label labelPlace;
         private System.Windows.Forms.PictureBox pictureBoxParking;
+        private System.Windows.Forms.ListBox listBoxParkings;
+        private System.Windows.Forms.Button buttonAddParking;
+        private System.Windows.Forms.Button buttonDeletePaking;
+        private System.Windows.Forms.TextBox textBoxNewLevelName;
+        private System.Windows.Forms.Label labelParkings;
     }
 }
--- WindowsFormsExcavator/WindowsFormsExcavator/ParkingForm.cs
+++ WindowsFormsExcavator/WindowsFormsExcavator/ParkingForm.cs
@@ -12,50 +12,54 @@ namespace WindowsFormsExcavator
 {
     public partial class ParkingForm : Form
     {
-        private readonly Parking<Excavator> parking;
+        private readonly ParkingCollection parkingCollection;
         public ParkingForm()
         {
             InitializeComponent();
-            parking = new Parking<Excavator>(pictureBoxParking.Width, pictureBoxParking.Height);
+            parkingCollection = new ParkingCollection(pictureBoxParking.Width, pictureBoxParking.Height);
             Draw();
+        }
+
+        public void ReloadLevels() 
+        {
+            int index = listBoxParkings.SelectedIndex;
+            listBoxParkings.Items.Clear();
+            for (int i = 0; i < parkingCollection.Keys.Count; i++)
+            {
+                listBoxParkings.Items.Add(parkingCollection.Keys[i]);
+            }
+            if (listBoxParkings.Items.Count > 0 && (index == -1 || index >= listBoxParkings.Items.Count))
+            {
+                listBoxParkings.SelectedIndex = 0;
+            }
+            else if (listBoxParkings.Items.Count > 0 && index > -1 && index < listBoxParkings.Items.Count)
+            {
+                listBoxParkings.SelectedIndex = index;
+            }
+
         }
 
         public void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            parking.Draw(gr);
-            pictureBoxParking.Image = bmp;
+            if (listBoxParkings.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parkingCollection[listBoxParkings.SelectedItem.ToString()].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
+
         }
 
         private void buttonParkingExcavator_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxParkings.SelectedIndex > -1)
             {
-                var excavator = new Excavator(100, 2000, dialog.Color);
-                if ((parking + excavator) != -1)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    Draw();
-                }
-                else
-                {
-                    MessageBox.Show("Парковка переполнена");
-                }
-            }
-        }
-
-        private void buttonParkingBucketExcavator_Click(object sender, EventArgs e)
-        {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
-                {
-                    var excavator = new BucketExcavavtor(100, 2500, dialog.Color, dialogDop.Color, true, true);
-
-                    if ((parking + excavator) != -1)
+                    var excavator = new Excavator(100, 1000, dialog.Color);
+                    if (parkingCollection[listBoxParkings.SelectedItem.ToString()] + excavator)
                     {
                         Draw();
                     }
@@ -64,15 +68,40 @@ namespace WindowsFormsExcavator
                         MessageBox.Show("Парковка переполнена");
                     }
                 }
-
             }
+        }
+
+        private void buttonParkingBucketExcavator_Click(object sender, EventArgs e)
+        {
+            if (listBoxParkings.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var bucketExcavator = new BucketExcavavtor(100, 1000, dialog.Color, dialogDop.Color, true, true);
+                    if (parkingCollection[listBoxParkings.SelectedItem.ToString()] + bucketExcavator)
+                        {
+                            Draw();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Парковка переполнена");
+                        }
+                    }
+                }
+            }
+
         }
 
         private void buttonGetExcavator_Click(object sender, EventArgs e)
         {
-            if (maskedTextBoxPlace.Text != "")
+            if (listBoxParkings.SelectedIndex > -1 && maskedTextBox.Text != "")
             {
-                var excavator = parking - Convert.ToInt32(maskedTextBoxPlace.Text);
+                var excavator = parkingCollection[listBoxParkings.SelectedItem.ToString()] -
+               Convert.ToInt32(maskedTextBox.Text);
                 if (excavator != null)
                 {
                     ExcavatorForm form = new ExcavatorForm();
@@ -81,6 +110,31 @@ namespace WindowsFormsExcavator
                 }
                 Draw();
             }
+        }
+
+        private void buttonAddParking_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNewLevelName.Text))
+            {
+                MessageBox.Show("Введите название парковки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            parkingCollection.AddParking(textBoxNewLevelName.Text);
+            ReloadLevels();
+
+        }
+
+        private void buttonDeletePaking_Click(object sender, EventArgs e)
+        {
+            if (listBoxParkings.SelectedIndex > -1)
+            {
+                if (MessageBox.Show($"Удалить парковку {listBoxParkings.SelectedItem.ToString()}?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    parkingCollection.DelParking(textBoxNewLevelName.Text);
+                    ReloadLevels();
+                }
+            }
+
         }
     }
 }
